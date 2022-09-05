a559c040c8b4463f1ab7a86c732c7ca6bbb293b1
Ayelen Barrale <ayelen@air-de-ayelen.localdomain>
2022-09-05 09:24:30 +0200

    styles and animation for main slider

---
+++ components/GeneralComponents/Slider/imagesSlider.tsx
@@ -0,0 +1,29 @@
+import sliderOne from '../../../public/assets/general/Lighthouse_Slider.webp'
+import sliderTwo from '../../../public/assets/general/Mut_Slider.webp'
+import sliderThree from '../../../public/assets/general/Suns_Race_Slider.webp'
+
+const images = [
+    {
+        id: 1,
+        src: sliderOne,
+        alt: "Lighthouse's Photo Manipulation Work",
+        width: '2048px',
+        height: '1152px'
+    },
+    {
+        id: 2,
+        src: sliderTwo,
+        alt: "Arquitect Studio's Branding Work",
+        width: '2048px',
+        height: '1152px'
+    },
+    {
+        id: 3,
+        src: sliderThree,
+        alt: "Enduro Race's Photo Manipulation Work",
+        width: '2048px',
+        height: '1152px'
+    }
+]
+
+export default images
--- components/GeneralComponents/Slider/index.tsx
+++ components/GeneralComponents/Slider/index.tsx
@@ -1,47 +1,67 @@
+import React from 'react'
 import dynamic from 'next/dynamic'
-import sliderOne from '../../../public/assets/general/Lighthouse_Slider.webp'
-import sliderTwo from '../../../public/assets/general/Mut_Slider.webp'
-import sliderThree from '../../../public/assets/general/Suns_Race_Slider.webp'
+import {LazyMotion, domAnimation, m} from 'framer-motion'
+import images from './imagesSlider'
 import style from '../../../styles/components/generalComponents/slider.module.scss'
 
 const Image = dynamic(() => import('next/image'))
 
 const Slider: React.FC = () => {
+  // const [index, setIndex] = useState(0)
+
+  // const handleNextSlide = () => {
+  //   if(index === images.length -1) {
+  //     setIndex(0)
+  //     return
+  //   }
+  //   setIndex(index + 1)
+  // }
+
+  // const handlePrevtSlide = () => {
+  //   if(index === 0) {
+  //     setIndex(images.length -1)
+  //     return
+  //   }
+  //   setIndex(index - 1)
+  // }
+
     return(
         <>
-          <section className={style.slider_container}>
-             <div id="slider_carousel" className={style.slider_carousel} >
-                <div className={style.slider_one}>
-                  <Image
-                        src={sliderOne}
-                        alt="Lighthouse's Photo Manipulation Work"
-                        priority
-                        width='2048px'
-                        height='1152px'
-                    />
-                </div>
-                <div className={style.slider_two}>
-                  <Image
-                          src={sliderTwo}
-                          alt="Arquitect Studio's Branding Work"
+          <div className={style.carousel}>
+            <div className={style.inner_carousel}>
+              {images.map((image) => {
+                return(
+                  <>
+                    <LazyMotion features= {domAnimation}>
+                      <m.div key={image.id} className={style.item} animate={{x: -2850}} transition={{repeat: Infinity, repeatType: "loop", repeatDelay: 1, duration: 10, delayChildren: 0.5}}>
+                        <Image 
+                          src={image.src}
+                          alt={image.alt}
                           priority
-                          width='2048px'
-                          height='1152px'
-                      />
-                </div>
-                <div className={style.slider_three}>
-                  <Image
-                          src={sliderThree}
-                          alt="Enduro Race's Photo Manipulation Work"
-                          priority
-                          width='2048px'
-                          height='1152px'
-                      />
-                </div>
-              </div>
-            </section>
+                          width={image.width}
+                          height={image.height}
+                        />
+                      </m.div>
+                    </LazyMotion>
+                  </>
+                )
+              })}
+              {/* <Image 
+                      src={images[index].src}
+                      alt={images[index].alt}
+                      priority
+                      width={images[index].width}
+                      height={images[index].height}
+                    /> */}
+            </div>
+            {/* <motion.div>
+              <button className={style.prev_button} onClick={handlePrevtSlide}>prev</button>
+              <button className={style.next_button} onClick={handleNextSlide}>next</button>
+            </motion.div> */}
+          </div>
         </>
     )
-}
-
-export default Slider
+  }
+  
+  export default Slider
+  
