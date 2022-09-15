d5d42cec17b68e59cbe1ffae9bb31f86288f0827
Ayelen Barrale <ayelen@air-de-ayelen.localdomain>
2022-09-15 17:50:32 +0200

    logic structure for contact form

--- components/GeneralComponents/FormContact/FormContact.tsx
+++ components/GeneralComponents/FormContact/FormContact.tsx
@@ -1,20 +1,33 @@
-import React from 'react';
+import { useState } from 'react'
 import style from '/styles/components/contact.module.scss'
 
-const FormContact: React.FC = () => {
+const FormContact = () => {
+    const [data, setData] = useState({
+        name: '',
+        email: '',
+        message: ''
+    })
+    const [clicked, setClicked] = useState(false)
+
+    const handleSubmit = () => {
+        setClicked(true)
+        console.log(data)
+    }
 
     return(
         <>
             <form className={style.form} >
                 <div className={style.form_row}>
-                    <input className={style.inputName}  placeholder='Name' />
-                    <input className={style.inputEmail}  placeholder='Email Address' />
-                    <input className={style.inputSubject}  placeholder='Subject' />
-                    <textarea className={style.txtMessage} cols={30} rows={8} placeholder='Message' />
-                    <button type='submit' value='submit' className={style.btnContact}>Send</button>
+                    <input className={style.inputName} required placeholder='Name' onChange={(value: any) => setData({...data, name : value.target.value})}/>
+                    <input className={style.inputEmail} required placeholder='Email Address' onChange={(value: any) => setData({...data, email : value.target.value})}/>
+                    <input className={style.inputSubject} required placeholder='Subject' />
+                    <textarea className={style.txtMessage} cols={30} rows={8} placeholder='Message' onChange={(value: any) => setData({...data, message : value.target.value})}/>
+                    <button type='submit' value='submit' className={style.btnContact} onClick={handleSubmit} >Send</button>
                 </div>
             </form>
             <div>
+                {clicked && <p>{`Thanks, your message was send. I'll contact you soon!`}</p>
+                }
                 <p id='thanxContact'></p>
             </div>
         </>
