0d9b5139499cef1e528aae0a9f3046a4e40cf8ad
Ayelen Barrale <ayelen@air-de-ayelen.localdomain>
2022-09-15 17:36:55 +0200

    contact form restyled

--- components/GeneralComponents/FormContact/FormContact.tsx
+++ components/GeneralComponents/FormContact/FormContact.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react';
 import style from '/styles/components/contact.module.scss'
 
 const FormContact: React.FC = () => {
@@ -7,15 +7,15 @@ const FormContact: React.FC = () => {
         <>
             <form className={style.form} >
                 <div className={style.form_row}>
-                    <input id="name"  placeholder="Name" />
-                    <input id="email"  placeholder="Email Address" />
-                    <input id="subject"  placeholder="Subject" />
-                    <textarea name="mensaje" id="mensaje" cols={30} rows={8} placeholder="Message" />
-                    <button type="submit" value='submit' id="btnEnviar">Send</button>
+                    <input className={style.inputName}  placeholder='Name' />
+                    <input className={style.inputEmail}  placeholder='Email Address' />
+                    <input className={style.inputSubject}  placeholder='Subject' />
+                    <textarea className={style.txtMessage} cols={30} rows={8} placeholder='Message' />
+                    <button type='submit' value='submit' className={style.btnContact}>Send</button>
                 </div>
             </form>
             <div>
-                <p id="thanxContact"></p>
+                <p id='thanxContact'></p>
             </div>
         </>
         
